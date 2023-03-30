f4d5326ff88afc16e6fd0b7ebd2e915a38a11186
Xen0f0n0 <sillebilstam@gmail.com>
2023-03-31 00:36:32 +0200

    2:00-2:30
    see description

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-let firstCard=11;
-let secondCard=11;
+let firstCard=10;
+let secondCard=4;
+
 let sum=firstCard+secondCard;
 let hasBlackJack=false;
 let isAlive=true;
 let message="";
+let messageEl=document.getElementById("message-el");
+let sumEl=document.querySelector("#sum-el");
+let cardsEl=document.querySelector("#cards-el");
+
 function startGame(){
+   renderGame();
+}
+
+function renderGame(){
+   cardsEl.textContent="Cards: "+ firstCard + " " + secondCard;
+   sumEl.textContent="Sum: "+sum;
+
  if (sum<=20){
     message="Do you want to draw a new card?";
  }else if(sum===21){
@@ -14,7 +26,20 @@ function startGame(){
     message="You lost!";
     isAlive=false;
  }
+   
+ messageEl.textContent=message;
+ 
+ 
+ 
+}
+
+function newCard(){
+   console.log("Drawing a new card");
+
 
  console.log(message);
+ let card=3;
+ sum += card;
+ renderGame();
 }
 
